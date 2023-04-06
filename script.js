74dda49b2d56c7c266b27b9290508f9b45de061f
AnandaBintang <anandabintang4@gmail.com>
2023-04-06 13:33:10 +0700

    new file:   img/logo.png 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ let hill1 = document.getElementById('hill1')
 let hill4 = document.getElementById('hill4')
 let hill5 = document.getElementById('hill5')
 
+let zoom = 1;
+const ZOOM_SPEED = 0.2;
+
 window.addEventListener('scroll', () => {
     let value = window.scrollY
-
-    text.style.marginTop = value * 2.5 + 'px'
 
     leaf.style.top = value * -1.5 + 'px'
     leaf.style.left = value * 1.5 + 'px'
@@ -15,4 +16,28 @@ window.addEventListener('scroll', () => {
     hill5.style.left = value * 1.5 + 'px'
     hill4.style.left = value * -1.5 + 'px'
     hill1.style.top = value * 1 + 'px'
+})
+
+window.addEventListener('wheel', (e) => {
+    if(e.deltaY > 0) {    
+        text.style.transition = 'transform 0.1s'
+        if(zoom <= 1) {
+            text.style.transform = `scale(${zoom -= ZOOM_SPEED})`;  
+        }
+    } else {    
+        text.style.transition = 'transform 0.1s'
+        if(zoom >= 0) {
+            text.style.transform = `scale(${zoom += ZOOM_SPEED})`;
+        }
+    }
+
+    if(zoom > 1) {
+        text.style.transform = `scale(1)`
+        zoom = 1
+    }
+    if(zoom < 0) {
+        text.style.transform = `scale(0)`
+        zoom = 0
+
+    }
 })
