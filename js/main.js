eba3c62b46ee3380a1a7ca894d37b4b016a8cd45
AnandaBintang <anandabintang4@gmail.com>
2023-04-12 11:15:32 +0700

    deleted:    js/animation.js 	modified:   js/main.js 	deleted:    js/preloader.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,70 @@
-document.addEventListener("DOMContentLoaded", () => {
-  preloader.style.visibility = "visible";
-  preloader.style.opacity = 1;
-  preloader.style.transition = "opacity 2s linear";
-
-  document.body.style.overflowY = "hidden";
+$(document).ready(function () {
+  // Preloader
+  $("body").css("overflowY", "hidden");
+  $("#preloader").css({
+    visibility: "visible",
+    opacity: "1",
+    transition: "opacity 2s linear",
+  });
 
   setTimeout(() => {
-    preloader.style.visibility = "hidden";
-    preloader.style.opacity = 0;
-    preloader.style.transition = "visibility 0s 0.3s, opacity 0.3s linear";
-    document.body.style.overflowY = "auto";
+    $("body").css("overflowY", "auto");
+    $("#preloader").css({
+      visibility: "hidden",
+      opacity: "0",
+      transition: "visibility 0s 0.3s, opacity 0.3s linear",
+    });
 
     setTimeout(() => {
-      preloader.remove();
+      $("#preloader").remove();
     }, 300);
   }, 5000);
+
+  // Parallax Animation
+  $("#leaf").css({ top: "-250px", left: "400px" });
+  $("#text").css({ scale: "1.5" });
+
+  setTimeout(() => {
+    // Leaf Animation
+    $("#leaf").css({
+      top: "0px",
+      left: "0px",
+      transition: "top 1.5s, left 1.5s",
+    });
+
+    // Text Animation
+    $("#text").css({
+      scale: "1",
+      transition: "scale 1s, top 1s",
+    });
+  }, 5000);
+
+  $(document).on("scroll", () => {
+    let value = window.scrollY;
+
+    $("#text").css({
+      transform: `scale(${1 - (value * 1.5) / 1000})`,
+    });
+
+    $("#leaf").css({
+      top: `${value * -1.5}px`,
+      left: `${value * 1.5}px`,
+      transition: "",
+    });
+
+    $("#hill5").css("left", `${value * 1.5}px`);
+    $("#hill4").css("left", `${value * -1.5}px`);
+    $("#hill1").css("top", `${value * 1}px`);
+  });
+
+  // Sticky Navbar
+  window.onscroll = function () {
+    myFunction();
+  };
 });
 
-window.onscroll = function () {
-  myFunction();
-};
-
+// Navigation Bar
 var navbar = document.getElementById("navbar");
-
 var sticky = navbar.offsetTop;
 
 function myFunction() {
