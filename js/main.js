4cb55eb194fad418e391760cf989122b22472769
AnandaBintang <anandabintang4@gmail.com>
2023-04-14 01:25:00 +0700

    modified:   index.html 	deleted:    js/jquery.drawsvg.js 	modified:   js/main.js 	new file:   js/tilt.js 	modified:   style.css 	new file:   test/index.html 	new file:   test/style.css

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
     setTimeout(() => {
       $("#preloader").remove();
     }, 300);
-  }, 5000);
+  }, 1);
 
   // Parallax Animation
   $("#leaf").css({ top: "-250px", left: "400px" });
@@ -61,43 +61,6 @@ $(document).ready(function () {
   window.onscroll = function () {
     stickyNavbar();
   };
-
-  // Services Parallax
-  const cards = document.querySelector(".cards");
-  const images = document.querySelectorAll(".card__img");
-  const backgrounds = document.querySelectorAll(".card__bg");
-  const range = 40;
-
-  // const calcValue = (a, b) => (((a * 100) / b) * (range / 100) -(range / 2)).toFixed(1);
-  const calcValue = (a, b) => ((a / b) * range - range / 2).toFixed(1); // thanks @alice-mx
-
-  let timeout;
-  document.addEventListener(
-    "mousemove",
-    ({ x, y }) => {
-      if (timeout) {
-        window.cancelAnimationFrame(timeout);
-      }
-
-      timeout = window.requestAnimationFrame(() => {
-        const yValue = calcValue(y, window.innerHeight);
-        const xValue = calcValue(x, window.innerWidth);
-
-        cards.style.transform = `rotateX(${yValue}deg) rotateY(${xValue}deg)`;
-
-        [].forEach.call(images, (image) => {
-          image.style.transform = `translateX(${-xValue}px) translateY(${yValue}px)`;
-        });
-
-        [].forEach.call(backgrounds, (background) => {
-          background.style.backgroundPosition = `${xValue * 0.45}px ${
-            -yValue * 0.45
-          }px`;
-        });
-      });
-    },
-    false
-  );
 
   // Image Slider
   $(".element").each(function () {
